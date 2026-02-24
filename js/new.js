604aefee149e6d86e90155a8c8581cb3190b2efc
Sean McKeown-Young <smckeown@advance.net>
2026-02-24 16:29:36 -0500

    commmmiterooo

---
+++ js/new.js
@@ -0,0 +1,7 @@
+<script>
+let str = document.getElementById("demo").innerHTML; 
+let res = str.replace(/blue/g, "red");
+document.getElementById("demo").innerHTML = res;
+</script>
+
+https://reader.theoregonian.com/
